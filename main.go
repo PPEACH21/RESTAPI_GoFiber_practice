c259e0b02329d5c41e862cc1cf83247a11cde8ca
PPEACH21 <sohizz1201@gmail.com>
2025-09-09 17:04:57 +0700

    add fiber

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
-func helloHandler(w http.ResponseWriter,r *http.Request){
-	if r.URL.Path != "/hello"{
-		http.Error(w,"404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if(r.Method != "GET"){
-		http.Error(w,"Method is not supported",http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w,"HELLO PPEACH")
-}
-
 func main(){
-	http.HandleFunc("/hello",helloHandler)
-	
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080",nil); err != nil{
-		log.Fatal(err)
-	}
+	app := fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error{
+		return c.SendString("HELLO PPEACH!")
+	})
+
+	app.Listen(":8080")
 }
